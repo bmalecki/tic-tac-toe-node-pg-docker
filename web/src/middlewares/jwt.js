560274539f10037935d3c07f01ed68050925ae14
Bartlomiej Malecki <b.s.malecki@gmail.com>
2018-06-26 00:42:45 +0200

    JWT

--- web/src/middlewares/jwt.js
+++ web/src/middlewares/jwt.js
@@ -1,6 +1,6 @@
 const koaJwt = require('koa-jwt');
 
-const isRevoked = (token, ctx, user) => Promise.resolve(false);
+const isRevoked = (ctx, decodedToken, token) => Promise.resolve(false);
 
 module.exports = koaJwt({
   secret: 'secret', // Should not be hardcoded
