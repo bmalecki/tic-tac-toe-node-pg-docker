ef4abdd0a52dc476ff5a5e131064237a6ac0a096
Bartlomiej Malecki <b.s.malecki@gmail.com>
2018-07-08 17:55:18 +0200

    io wip

--- client/src/reducers/socketio.js
+++ client/src/reducers/socketio.js
@@ -5,6 +5,8 @@ export default (state = defaultState, action) => {
   switch (action.type) {
     case 'INIT_SOCKET_IO':
       return action.payload.socket;
+    case 'DESTROY_SOCKET_IO':
+      return null;
     default:
       return state;
   }
