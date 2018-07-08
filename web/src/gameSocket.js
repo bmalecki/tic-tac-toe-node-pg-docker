678d11a027171d00f5c40153a3fea76b9d9c90cd
Bartlomiej Malecki <b.s.malecki@gmail.com>
2018-07-09 00:38:32 +0200

    wip;

--- web/src/gameSocket.js
+++ web/src/gameSocket.js
@@ -5,5 +5,8 @@ module.exports = (http) => {
 
   io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
   });
 };
